7649095a9cc5db1b7960e14561170faa318a1832
Nina-S-Mathew <99273767+Nina-S-Mathew@users.noreply.github.com>
2022-06-22 11:02:30 -0400

    green light

--- main.ts
+++ main.ts
@@ -4,15 +4,17 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Green))
-    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Giraffe)
+    basic.pause(100)
 })
 input.onButtonPressed(Button.B, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.showIcon(IconNames.Sad)
+    basic.pause(100)
 })
 let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P1, 36, NeoPixelMode.RGB)
-strip.showRainbow(1, 360)
-strip.shift(1)
+strip = neopixel.create(DigitalPin.P1, 36, NeoPixelMode.RGB_RGB)
+strip.showRainbow(0, 255)
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     strip.rotate(1)
