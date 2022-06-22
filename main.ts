f83398188427b8d5e47e05105823f0673e657191
Nina-S-Mathew <99273767+Nina-S-Mathew@users.noreply.github.com>
2022-06-22 10:48:54 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,20 @@
 input.onButtonPressed(Button.A, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    basic.showIcon(IconNames.Happy)
 })
 input.onButtonPressed(Button.AB, function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.showIcon(IconNames.Happy)
 })
 input.onButtonPressed(Button.B, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Red))
 })
 let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P1, 37, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P1, 36, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
+strip.shift(1)
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     strip.rotate(1)
     basic.pause(200)
-    strip.show()
 })
